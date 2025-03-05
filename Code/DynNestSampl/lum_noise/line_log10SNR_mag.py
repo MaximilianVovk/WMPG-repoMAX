0e57bd2c592177c84e3aeddc98c3e8751e3f1765
MaximilianVovk <mvovk@uwo.ca>
2025-03-05 12:45:06 -0500

    change the title of the plots and labels and putt mean and st dev of the DRA CAP ORI

--- Code/DynNestSampl/lum_noise/line_log10SNR_mag.py
+++ Code/DynNestSampl/lum_noise/line_log10SNR_mag.py
@@ -63,6 +63,7 @@ def process_snr_files(input_dir):
         f.write("# All regression line functions (after outlier removal):\n\n")
 
     zscore_threshold = 3
+    sigma_threshold = 2
 
     for snr_file in snr_files:
         x_vals = []
@@ -90,6 +91,23 @@ def process_snr_files(input_dir):
             print(f"No valid data in {snr_file}. Skipping.")
             continue
 
+        # check if all the values are the same
+        if np.all(x_vals == x_vals[0]) or np.all(y_vals == y_vals[0]):
+            print(f"All values are the same in {snr_file}. Skipping.")
+            continue
+
+        # delete inf values of x_inliers on both x and y
+        y_vals = y_vals[np.isfinite(x_vals)]
+        x_vals = x_vals[np.isfinite(x_vals)]
+        # delete nan values of y_inliers on both x and y
+        x_vals = x_vals[np.isfinite(y_vals)]
+        y_vals = y_vals[np.isfinite(y_vals)]
+
+        # make an inlier_mask that is all True
+        inlier_mask = np.ones(len(x_vals), dtype=bool)
+
+        ################# zscore remove outliers #################
+
         # # Initial fit
         # m_init, c_init = np.polyfit(x_vals, y_vals, 1)
         # y_fit_init = m_init * x_vals + c_init
@@ -115,21 +133,39 @@ def process_snr_files(input_dir):
         # x_inliers = x_vals[inlier_mask]
         # y_inliers = y_vals[inlier_mask]
 
-        x_inliers = -2.5*np.array(x_vals)
-        y_inliers = np.array(y_vals)
+        # # x_inliers = -2.5*np.array(x_vals)
+        # x_inliers = np.array(x_vals)
+        # y_inliers = np.array(y_vals)
 
-        # check if all the values are the same
-        if np.all(x_inliers == x_inliers[0]) or np.all(y_inliers == y_inliers[0]):
-            print(f"All values are the same in {snr_file}. Skipping.")
-            continue
+        ################# line sigma remove outliers #################
 
-        # delete inf values of x_inliers on both x and y
-        y_inliers = y_inliers[np.isfinite(x_inliers)]
-        x_inliers = x_inliers[np.isfinite(x_inliers)]
-        # delete nan values of y_inliers on both x and y
-        x_inliers = x_inliers[np.isfinite(y_inliers)]
-        y_inliers = y_inliers[np.isfinite(y_inliers)]
+        # # 1) Initial linear regression
+        # m_init, c_init = np.polyfit(x_vals, y_vals, 1)
+        # y_fit_init = m_init * x_vals + c_init
 
+        # # 2) Identify outliers
+        # residuals = y_vals - y_fit_init
+        # std_res = np.std(residuals)
+        # # Indices of points that are within the threshold
+        # inlier_mask = np.abs(residuals) <= sigma_threshold * std_res
+
+        # # Filter data to remove outliers
+        # x_inliers = x_vals[inlier_mask]
+        # y_inliers = y_vals[inlier_mask]
+
+        # # If everything got removed as outliers, skip
+        # if len(x_inliers) == 0:
+        #     print(f"All points were outliers in {snr_file}. Skipping plot.")
+        #     continue
+
+        # # 3) Final linear regression on inliers only
+        # m_final, c_final = np.polyfit(x_inliers, y_inliers, 1)
+        # y_fit_final = m_final * x_inliers + c_final
+
+        ###########################################################
+
+        x_inliers = x_vals[inlier_mask]
+        y_inliers = y_vals[inlier_mask]
 
         # Final fit on inliers
         m_final, c_final = np.polyfit(x_inliers, y_inliers, 1)
@@ -139,12 +175,31 @@ def process_snr_files(input_dir):
 
         # Plot
         plt.figure()
-        plt.scatter(x_inliers, y_inliers, label='Inliers')
+        plt.scatter(x_inliers, y_inliers, label='Data')
         plt.plot(x_inliers, y_fit_final, label=f'y = {m_final:.4f}x + {c_final:.4f}')
         # use the latex format for the labels for the x and y axis
-        plt.xlabel(r'$-2.5\times log_{10}(SNR)$')
-        plt.ylabel('magnitude')
-        plt.title('log10SNR vs mag_data')
+        # plt.xlabel(r'$-2.5\times log_{10}(SNR)$')
+        plt.xlabel(r'$log_{10}(SNR)$')
+        plt.ylabel('Apparent Magnitude')
+        # take the file name from snr_file
+        file_name = os.path.basename(snr_file)
+        # for the title take the name of the file all the way with either tavis or elgin isode like for 2019-07-26_03_08_46elgin_SNR_values.png only 2019-07-26_03_08_46elgin
+        title_name = ""
+        if "tavis" in file_name:
+            title_name = file_name.split("tavis")[0]
+            title_name = title_name + "tavis"
+        elif "elgin" in file_name:
+            title_name = file_name.split("elgin")[0]
+            title_name = title_name + "elgin"
+        # delete the - from the title_name
+        title_name = title_name.replace("-", "")
+        # delete all the _ but keep the first one
+        title_name = title_name.replace("_", "")
+        # in between the 8 and the 9 character if is long enougth put a _ to separate the date from the time
+        if len(title_name) > 8:
+            title_name = title_name[:8] + "_" + title_name[8:]
+        plt.title(title_name)
+        # plt.title('log10SNR vs mag_data')
         plt.legend()
         plt.grid(True)
 
@@ -189,6 +244,6 @@ def process_snr_files(input_dir):
 if __name__ == "__main__":
     # Example usage:
     # Replace 'path/to/directory' with the directory containing your SNR_values.txt files
-    directory_with_txt = "/home/mvovk/Documents/2ndPaper/Reductions/ORI"
+    directory_with_txt = "/srv/meteor/reductions/emccd/EMCCD_showers/DRA"
     run_skyfit_on_all_states(directory_with_txt)
     process_snr_files(directory_with_txt)
